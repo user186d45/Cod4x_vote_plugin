2c1c1160ef50a636914e3e2686a8058333e2277f
user186d45 <dowhcosv@protonmail.com>
2025-03-10 00:59:21 +0330

    Add files via upload

---
+++ Cod4x_Vote_Plugin_v1.0.1/build/default/CMakeFiles/Cod4x_Vote_Plugin_v1.0.1.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for Cod4x_Vote_Plugin_v1.0.1.
---
+++ Cod4x_Vote_Plugin_v1.0.1/build/default/CMakeFiles/Cod4x_Vote_Plugin_v1.0.1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cod4x_Vote_Plugin_v1.0.1.
+# This may be replaced when dependencies are built.
